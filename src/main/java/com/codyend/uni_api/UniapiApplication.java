daf518465cf85b749871442bcdac3ec220ca66ac
Nicoya505 <figueroablanco92@gmail.com>
2024-10-18 00:43:52 +0200

    feat(api): Add-initial-structure

---
+++ src/main/java/com/codyend/uni_api/UniapiApplication.java
@@ -0,0 +1,13 @@
+package com.codyend.uni_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniapiApplication.class, args);
+	}
+
+}
